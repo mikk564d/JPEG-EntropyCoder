562d773afb9f0b27201c8199974771943fd676be
Mikkel Nygaard <mnygaa15@student.aau.dk>
2016-04-11 11:15:21 +0200

    Fix

--- JPEG-EntropyCoder/JPEG-EntropyCoder/Interfaces/IJPEGFileHandler.cs
+++ JPEG-EntropyCoder/JPEG-EntropyCoder/Interfaces/IJPEGFileHandler.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace JPEG_EntropyCoder {
     interface IJPEGFileHandler {
-
+        string DQT { get; set; }
+        string DHT { get; set; }
+        string SOF { get; set; }
+        string SOS { get; set; }
+        string CompressedImage { get; set; }
+        string All { get; set; }
+        void LoadFile(string path);
+        void SaveFile(string path);
     }
 }
