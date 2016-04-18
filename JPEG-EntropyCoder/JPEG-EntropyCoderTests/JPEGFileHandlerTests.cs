9afd8d71fe5f7edc543d9f99447d04bf925ef192
Jalf <benjaminboendergaard@hotmail.com>
2016-04-18 08:57:02 +0200

    Added unit test project

---
+++ JPEG-EntropyCoder/JPEG-EntropyCoderTests/JPEGFileHandlerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace JPEG_EntropyCoderTests
+{
+    [TestFixture]
+    public class JPEGFileHandlerTests
+    {
+        [Test]
+        public void RemoveDashes_SimpleValue_Calculated()
+        {
+
+        }
+    }
+}
